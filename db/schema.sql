2bd0abf99cf46177e329925cfcd1a96993971749
LDegraz <degrazial1@yahoo.com>
2025-01-12 08:57:31 -0500

    added code to schema file

--- db/schema.sql
+++ db/schema.sql
@@ -9,8 +9,8 @@ CREATE TABLE department (
 CREATE TABLE role (
   id SERIAL PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
-  salary DECIMAL,
-  department INTEGER,
+  salary DECIMAL NOT NULL,
+  department INTEGER NOT NULL,
   FOREIGN KEY (department)
   REFERENCES department(id)
   ON DELETE SET NULL
@@ -20,7 +20,7 @@ CREATE TABLE employee (
   id SERIAL PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id INTEGER,
+  role_id INTEGER NOT NULL,
   manager_id INTEGER,
   FOREIGN KEY (role_id)
   REFERENCES role(id),
